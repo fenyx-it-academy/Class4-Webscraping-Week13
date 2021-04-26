22b6e3c6f0d6b91c92282ce310ad7f7cabf5093b
caliskanbulent <55192161+caliskanbulent@users.noreply.github.com>
2021-04-26 15:35:38 +0200

    Update araba.py

--- araba.py
+++ araba.py
@@ -1,39 +1,66 @@
 import scrapy
 
 
-class ArabaSpider(scrapy.Spider):
-    name = 'araba'
-    allowed_domains = ['arabam.com/ikinci-el/otomobil/bmw']
-    start_urls = ['https://www.arabam.com/ikinci-el/otomobil/bmw']
+
+class AutoSpider(scrapy.Spider):
+    name = 'Auto'
+    # allowed_domains = ['arabam.com']
+    start_urls = ["https://www.arabam.com/ikinci-el/otomobil"]
 
     def parse(self, response):
-        hrefbmw = response.css("h3.crop-after a ::attr(href)").extract()
-        
-        for href in hrefbmw:
-            url = response.urljoin(href)
-            print("--------------------------------------------------------------------------")
-            print(url)
+        # extract() metodu ise bize bir liste return eder. get()/getall() ilk buldugu sonucu/hepsini return eder.        
+
+        BMW = response.xpath('//*[@id="js-hook-for-listen-scroll-cta"]/div[1]/ul[2]/li/ul/li[6]/a/@href').get() # BMW
+        CITROEN = response.xpath('//*[@id="js-hook-for-listen-scroll-cta"]/div[1]/ul[2]/li/ul/li[11]/a/@href').get()  # citroen
+        HONDA = response.xpath('//*[@id="js-hook-for-listen-scroll-cta"]/div[1]/ul[2]/li/ul/li[21]/a/@href').get() # honda
+
+        for href in [BMW,CITROEN,HONDA]:               
+            url = response.urljoin(href)                                
+            yield scrapy.Request(url, callback=self.parse_page)            
+
+    def parse_page(self,response):          
+        liste = response.css('tbody tr')             
+        for i in liste:            # .css('td a ::text').getall()
+            model=i.css("td:nth-child(2) h3 a ::text").get()  
+            if model==None:
+                model='undefined'                        
+            year=i.css("td:nth-child(4) div a ::text").get() 
+            if year==None:
+                year=2050
+            else:
+                year=int(year)
+            km=i.css("td:nth-child(5) div a ::text").get()  
+
+            if km:
+                km=km.split('.')
+                km=''.join(km)
+                km=int(km)     
+            else:
+                km=1000000   
+            color=i.css("td:nth-child(6) div a ::text").get()   
+            if color==None: 
+                color=''          
+            price=i.css("td:nth-child(7) div span a ::text").get() 
+            if price==None:
+                price=1000000
+            else:
+                price=price.split('.')
+                price=''.join(price)
+                price=price.split('TL')
+                price=''.join(price)
+                price=int(price)           
+
+            city=i.css("td:nth-child(9) div div:nth-child(1) a span:nth-child(1) ::text").get()  
+            if city==None: 
+                city='undefined'                      
+            yield {"model":model,"year":year,"km":km,"color":color,"price":price,"city":city}   
+
+        # next butonu xpath i
+        next_page = response.xpath("//*[@id='pagingNext']/@href")    
+
+        # eger next sayfasi var ise bu bloga girer. yoksa girmez.      
+        if next_page:                                                              
+            url = response.urljoin(next_page.get())           
             yield scrapy.Request(url, callback=self.parse_page)
-
-            next_page = response.css("#pagingNext ::attr(href)").extract()
-            print(next_page)
-
-            if next_page:
-                url = response.urljoin(next_page[0])
-                yield scrapy.Request(url)
-
-    def parse_page(self, response):
-            model=li.css("td:nth-child(2) h3 a ::text").get()
-            
-            yil=li.css("td:nth-child(4) div a ::text").get()
-           
-            kilometre=li.css("td:nth-child(5) div a ::text").get()
-            
-            renk=li.css("td:nth-child(6) div a ::text").get()
-            
-            fiyat=li.css("td:nth-child(7) div span a ::text").get()
-            
-            il=li.css("td:nth-child(9) div div:nth-child(1) a span:nth-child(1) ::text").get()
-                
-            print(model,yil,kilometre,renk,fiyat,il)
+        
 

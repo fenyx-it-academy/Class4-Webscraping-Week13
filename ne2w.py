67b933f4315617827e6fb566f6f481c8065dfeaa
abdullahkbc <61469978+abdullahkbc@users.noreply.github.com>
2021-04-27 20:40:52 +0200

    Add files via upload

---
+++ ne2w.py
@@ -0,0 +1,62 @@
+import scrapy
+
+
+class Ne2wSpider(scrapy.Spider):
+    name = 'ne2w'
+    start_urls = ['https://www.arabam.com/ikinci-el/otomobil/']
+
+    # model yil km renk fiyat il
+    def parse(self, response):
+        BMW = response.xpath('//*[@id="js-hook-for-listen-scroll-cta"]/div[1]/ul[2]/li/ul/li[6]/a/@href').get()
+        HONDA = response.xpath('//*[@id="js-hook-for-listen-scroll-cta"]/div[1]/ul[2]/li/ul/li[21]/a/@href').get()
+        CITROEN = response.xpath('//*[@id="js-hook-for-listen-scroll-cta"]/div[1]/ul[2]/li/ul/li[11]/a/@href').get()
+
+        for auto in [BMW, HONDA, CITROEN]:
+            url = response.urljoin(auto)
+            yield scrapy.Request(url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        # arabalar = response.xpath('//*[@id="main-listing"]/tbody')
+        arabalar = response.css('tbody tr')
+        for i in arabalar:  # .css('td a ::text').getall()
+            model = i.css("td:nth-child(2) h3 a ::text").get()
+            if model == None:
+                model = 'undefined'
+            year = i.css("td:nth-child(4) div a ::text").get()
+            if year == None:
+                year = 2050
+            else:
+                year = int(year)
+            km = i.css("td:nth-child(5) div a ::text").get()
+
+            if km:
+                km = km.split('.')
+                km = ''.join(km)
+                km = int(km)
+            else:
+                km = 1000000
+            color = i.css("td:nth-child(6) div a ::text").get()
+            if color == None:
+                color = ''
+            price = i.css("td:nth-child(7) div span a ::text").get()
+            if price == None:
+                price = 1000000
+            else:
+                price = price.split('.')
+                price = ''.join(price)
+                price = price.split('TL')
+                price = ''.join(price)
+                price = int(price)
+
+            city = i.css("td:nth-child(9) div div:nth-child(1) a span:nth-child(1) ::text").get()
+            if city == None:
+                city = 'undefined'
+            yield {"model": model, "year": year, "km": km, "color": color, "price": price, "city": city}
+            # print(model, year, color, price)
+            # next butonu xpath i
+        next_page = response.xpath("//*[@id='pagingNext']/@href")
+
+        # eger next sayfasi var ise bu bloga girer. yoksa girmez.
+        if next_page:
+            url = response.urljoin(next_page.get())
+            yield scrapy.Request(url, callback=self.parse_page)
